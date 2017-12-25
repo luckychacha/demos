bd8e8fe3c50b92ff96f08e15e4163bf786b8bc90
leixinxin <leixinxin@leying365.com>
2017-12-25 13:28:37 +0800

    package

--- react/webpack.config.js
+++ react/webpack.config.js
@@ -15,7 +15,7 @@ module.exports = {
         loader: 'babel-loader',
         exclude: /node-module/,
         query: {
-    		  presets: ['es2015', 'react']
+    		  presets: ['env', 'react']
     	  }
       },
       {
